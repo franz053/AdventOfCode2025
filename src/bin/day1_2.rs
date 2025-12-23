add784c8a25a649c30fa65ee77c5c68d98814d51
franz053 <139059326+franz053@users.noreply.github.com>
2025-12-23 15:28:29 +0100

    day1_2.rs created

--- src/bin/day1_2.rs
+++ src/bin/day1_2.rs
@@ -1,5 +1,4 @@
 use std::fs;
-
 fn main() {
     let file_path = "inputs/input1.txt";
 
@@ -14,11 +13,19 @@ fn main() {
         let num: i32 = word[1..].parse::<i32>().unwrap(); //amount to turn
 
         current_value += if dir { num } else { -num };
-        current_value = ((current_value % 100) + 100) % 100; //wrap into range 0-99
 
         if current_value == 0 {
             result += 1;
+        } else if current_value >= 100 {
+            result += current_value / 100;
+        } else if current_value < 0 {
+            if current_value + num != 0 {
+                //check if value was 0 before, and don't count it twice if it is
+                result += 1
+            }
+            result += current_value / -100;
         }
+        current_value = ((current_value % 100) + 100) % 100; //wrap into range 0-99
     }
     println!("{}", result);
 }
